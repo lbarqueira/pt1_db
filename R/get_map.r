0ca0eac4aba42d50e4f19a7bfefa565bfe9a925f
lbarqueira <luis.barqueira@gmail.com>
2024-05-26 03:45:10 +0100

    update 26-5-2024

--- R/get_map.r
+++ R/get_map.r
@@ -3,11 +3,11 @@
 ############################################################################
 
 
-#'Load 100km2 gridded hexagonal map for Mainland Portugal
-#'
-#'Function to add the gridded hexagonal map of Mainland Portugal.
-#'This function has no argument
-get_map <- function() {
+# Load 100km2 gridded hexagonal map for Mainland Portugal
+#
+# Function to add the gridded hexagonal map of Mainland Portugal.
+# This function has no argument
+get_map <- function(type = "hex_100") {
   hex <- sf::read_sf(
     "https://raw.githubusercontent.com/lbarqueira/pt1_db/main/maps/hex_cropped_portugal_100km2.gpkg"
   )
