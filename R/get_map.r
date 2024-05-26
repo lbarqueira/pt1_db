31f22af2b97e5cfb1565ef30f316f7621fd7f98f
lbarqueira <luis.barqueira@gmail.com>
2024-05-26 04:06:35 +0100

    update 26-5-2024

--- R/get_map.r
+++ R/get_map.r
@@ -1,15 +1,31 @@
 ############################################################################
-#   FUNCTION    get_map()        HEX MAP 100km2 LOAD from GitHub
+#   FUNCTION    get_map(type)     HEX MAP 100km2 LOAD from GitHub by default
 ############################################################################
 
 
-# Load 100km2 gridded hexagonal map for Mainland Portugal
+# Load 100km2 gridded hexagonal map for Mainland Portugal by default
 #
-# Function to add the gridded hexagonal map of Mainland Portugal.
-# This function has no argument
+# Function to add the gridded hexagonal map of Mainland Portugal by default.
+# This function has the argument "type" for other map types:
+# -
+# -
+# -
+# -
+# -
+
+
 get_map <- function(type = "hex_100") {
-  hex <- sf::read_sf(
-    "https://raw.githubusercontent.com/lbarqueira/pt1_db/main/maps/hex_cropped_portugal_100km2.gpkg"
-  )
-  return(hex)
+  tb <- tibble(
+    name = c(
+      "hex_100" #1 - Load 100km2 gridded hexagonal map for Mainland Portugal
+    ),
+    url = c(
+      "https://raw.githubusercontent.com/lbarqueira/pt1_db/main/maps/hex_cropped_portugal_100km2.gpkg" #1 # nolint
+    )
+  ) |>
+    filter(name == type)
+
+  ext <- tb$url
+
+  return(ext)
 }
